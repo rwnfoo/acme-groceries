bf835067b90f436a664dba7ed6d967a8e780dfe0
Rowena Foo <rwnfoo@github.com>
2021-09-02 19:49:17 -0500

    add combine reducers

--- src/store.js
+++ src/store.js
@@ -1,24 +1,45 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const initialState = {
-  groceries: [],
-  view: ''
-};
-const store = createStore((state = initialState, action)=> {
-  if(action.type === 'LOAD'){
-    state = {...state, groceries: action.groceries };
+const LOAD = "LOAD"
+const UPDATE = "UPDATE"
+const CREATE = "CREATE"
+const SET_VIEW = "SET_VIEW"
+
+// const initialState = {
+//   groceries: [],
+//   view: ''
+// };
+
+const groceriesReducer = (state = [], action) => {
+  if(action.type === LOAD){
+    // state = {...state, groceries: action.groceries };
+    state = action.groceries;
   }
-  if(action.type === 'UPDATE'){
-    state = {...state, groceries: state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )};
+  if(action.type === UPDATE){
+    //state = {...state, groceries: state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )};
+    state = state.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )
   }
-  if(action.type === 'CREATE'){
-    state = {...state, groceries: [...state.groceries, action.grocery ]}
-  }
-  if(action.type === 'SET_VIEW'){
-    state = {...state, view: action.view};
+  if(action.type === CREATE){
+    //state = {...state, groceries: [...state.groceries, action.grocery ]}
+    state = [...state, action.grocery]
   }
   return state;
+}
+
+const viewReducer = (state = '', action) => {
+  if(action.type === SET_VIEW){
+    //state = {...state, view: action.view};
+
+  }
+  return state;
+}
+
+const reducers = combineReducers({
+  groceries: groceriesReducer,
+  view: viewReducer
 });
+
+const store = createStore(reducers);
 
 export default store;
 
